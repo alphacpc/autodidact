ad358f814f71509cdbc89bcfd841cac863dab093
alphacpc <alphacpc@gmail.com>
2022-02-05 18:06:33 +0000

    Optimize module less line code

--- Projet_python_sa/render_tabs_etudiants.py
+++ Projet_python_sa/render_tabs_etudiants.py
@@ -1,44 +1,26 @@
-import csv
+import csv;
 
 Tab = [];
-fieldnames = [];
 
 def return_tabs(filename):    
     
+    fieldnames = [];
+    count = 0;
 
     with open(filename) as csv_file:
 
         content = csv.reader(csv_file);
-        fieldnames = [];
-        Tab = [];
-        count = 0;
-
-
-
+        
         for row in content:
-            
-            # if count == 1:
-            #     fieldnames = row
-            
-            # elif count > 2:
-            #     dictionnaire = dict(zip(fieldnames,row))
-            #     Tab.append(dictionnaire);
-
-            # count += 1
-
-            # if count >= 10:
-            #     csv_file.close();   
-            
-            #     return Tab;
 
             if count < 10:
                 if count == 1:
-                    fieldnames = row
+                    fieldnames = row;
                     
                 elif count >= 2:
-                    dictionnaire = dict(zip(fieldnames,row))
+                    dictionnaire = dict(zip(fieldnames,row));
                     Tab.append(dictionnaire);  
 
-            count += 1  
+            count += 1;
             
         return Tab;
