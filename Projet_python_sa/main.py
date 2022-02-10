c7ff2ae7034fe03074f290a3b7d9d9e33779af5f
alphacpc <alphacpc@gmail.com>
2022-02-10 18:25:59 +0000

    Updated top five students

--- Projet_python_sa/main.py
+++ Projet_python_sa/main.py
@@ -9,36 +9,36 @@ tab_valid, tab_invalid = terminator.separate_students_with_tab(all_students);
 
 
 
-# fonctions.welcome();
+fonctions.welcome();
 
-# xEntre = input("Donner: ")
+xEntre = input("Donner: ")
 
-# while int(xEntre) > 1 and int(xEntre) > 6:
-#     xEntre = input("Donner valable: ")
+while int(xEntre) > 1 and int(xEntre) > 6:
+    xEntre = input("Donner valable: ")
     
 
-# if int(xEntre) == 1:
-#     fonctions.get_all_elements_tabs(all_students,"Toutes les informations");
+if int(xEntre) == 1:
+    fonctions.get_all_elements_tabs(all_students,"Toutes les informations");
 
 
-# elif int(xEntre) == 2:
-#     fonctions.get_all_elements_tabs(tab_valid, "Les informations valides")
+elif int(xEntre) == 2:
+    fonctions.get_all_elements_tabs(tab_valid, "Les informations valides")
     
 
-# elif int(xEntre) == 3:
-#     fonctions.get_all_elements_tabs(tab_invalid, "Les informations invalides")
+elif int(xEntre) == 3:
+    fonctions.get_all_elements_tabs(tab_invalid, "Les informations invalides")
     
 
-# elif int(xEntre) == 4:
-#     fonctions.get_element_by_numero('40DKG6T',all_students)
+elif int(xEntre) == 4:
+    fonctions.get_element_by_numero('40DKG6T',all_students)
 
 
-# elif int(xEntre) == 5:
-#     fonctions.top_five(tab_valid);
+elif int(xEntre) == 5:
+    fonctions.top_five(tab_valid);
 
 
-# elif int(xEntre) == 6:
-#     fonctions.stats(all_students,tab_valid,tab_invalid);
+elif int(xEntre) == 6:
+    fonctions.stats(all_students,tab_valid,tab_invalid);
 
 
 
@@ -67,18 +67,18 @@ tab_valid, tab_invalid = terminator.separate_students_with_tab(all_students);
 # [1,4,9].sort()
 
 
-count = 0
-def by_moyenne(entry):
-    return entry["Moyenne Generale"];
+# count = 0
+# def by_moyenne(entry):
+#     return entry["Moyenne Generale"];
 
-tab_valid.sort(key=by_moyenne,reverse=True)
+# tab_valid.sort(key=by_moyenne,reverse=True)
 
-for row in tab_valid:
+# for row in tab_valid:
     
-    if count < 5:
-        rang = count+1
-        print("Range",rang," est : ",row,'\n')
-    count +=1
+#     if count < 5:
+#         rang = count+1
+#         print("Range",rang," est : ",row,'\n')
+#     count +=1
 
 
 # for item in tab_valid:
