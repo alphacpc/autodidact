303c2885a1f589375322c81b46b3f3c6bfe5987b
alphacpc <alphacpc@gmail.com>
2022-02-14 11:44:55 +0000

    finish project

--- R_openclassroom/project.r
+++ R_openclassroom/project.r
@@ -28,4 +28,26 @@ summary(mondf[28:54,])
 summary(mondf[1:28,])
 
 
+which(mondf==20, arr.ind = T)
+mondf[49,]
+mondf[53,]
+mondf[54,]
 
+
+#Pour voir l'élève qui as 20
+View(mondf[mondf$notes==20,])
+
+
+#Pour voir la moyenne des filles
+mean(mondf[mondf$sexe == 'F', 'notes'], na.rm=TRUE)
+
+#Pour voir la moyenne des garçons
+mean(mondf[mondf$sexe == 'M', 'notes'], na.rm=TRUE)
+
+
+
+
+
+#Pour voir la moyenne des devoir de Maths et SVT
+mean(mondf[mondf$matiere == 'Math', 'notes'], na.rm=T)
+mean(mondf[mondf$matiere == 'SVT', 'notes'], na.rm=T)
