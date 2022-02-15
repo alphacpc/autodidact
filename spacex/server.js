b611facf36eb466c437ff07cbe0e09d826c9f1ef
alphacpc <alphacpc@gmail.com>
2022-02-15 16:48:32 +0000

    Added cors on server.js fileC

--- spacex/server.js
+++ spacex/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const {graphqlHTTP} = require("express-graphql");
 const schema = require("./schema.js");
 
 const app = express();
+
+//MIDDLEWARE CORS
+app.use(cors);
 
 app.use('/graphql', graphqlHTTP({
     schema,
