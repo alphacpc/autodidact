0d5b0b83a34dc04069869d5b80d7a79fb5cf02bb
alphacpc <alphacpc@gmail.com>
2022-02-19 15:18:18 +0000

    Started create function for separeted row

--- started_r_lang/project_data.r
+++ started_r_lang/project_data.r
@@ -1,22 +1,53 @@
-setwd("/home/alpha/Projects/Learning/R_openclassroom/")
-df = read.csv("datas.csv")
+setwd("/home/alpha/Projects/Learning/started_r_lang")
+df = read.csv("datas/datas.csv")
 
-labels = c("Math","PC","SVT","Anglais","HG","Français")
-fac = factor(labels)
+#Create factor for label matieres
+labels = c("Math","PC","SVT","Anglais","HG","Francais")
+mat_fac = factor(labels)
+length(mat_fac)
 
-notes = data.frame(df$Note)
-notes
+#Recup col notes to dataframe
+col_notes = data.frame(df$Note)
 
-single = notes[1,]
-single
+# Job on rows 4 of df
+row_four = col_notes[4,]
+row_four_list = strsplit(row_four, "#")
 
-sep = strsplit(single, split = "#")
-sep
+#Get single matiere
+get_math = row_four_list[[1]][1]
 
-length(sep[[1]])
+#Split get_mat
+list_math = strsplit(get_math, "\\[")
+label_mat = list_math[[1]][1]
+notes_mat = list_math[[1]][2]
+get_note_mat = strsplit(notes_mat,"]")[[1]][1]
 
-for (el in sep){
-  print(el)
+
+#Split notes mat
+devoirs = strsplit(strsplit(get_note_mat,":")[[1]][1],";")
+comp = strsplit(get_note_mat,":")[[1]][2]
+devoirs[[1]][2]
+
+
+#Define function to split all rows
+row_spliting <- function(row){
+  row_list = strsplit(row_four, "#")
+  for (mat in row_list){
+    #print(mat)
+    label_matiere = strsplit(mat,"\\[")[[1]][1]
+    notes_matiere = strsplit(strsplit(mat,"\\[")[[1]][2],"]")[[1]][1]
+    devoirs = strsplit(strsplit(notes_matiere,":")[[1]][1],";")
+    comp = strsplit(notes_matiere,":")[[1]][2]
+    print(label_matiere)
+    print(devoirs)
+    print(comp)
+  }
+}
+
+row_spliting("Math[04;13:05] #Francais[15;16:14] #Anglais[15;16:11] #PC[04;03:07] #SVT[12;09:11] #HG[16;15:10]")
+#Loop row for getting infos
+for (element in row_four_list){
+  print(element)
 }
 
 
---
+++ started_r_lang/test.r
@@ -0,0 +1,7 @@
+helloWord <- function(){ 
+  for(i in c("12","34", "100", "200")){
+    print(i)
+  }
+}
+
+helloWord()
