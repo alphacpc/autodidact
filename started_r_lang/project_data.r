f1a16033398d228608941ada01ed34599faba23d
alphacpc <alphacpc@gmail.com>
2022-02-19 18:03:40 +0000

    on way to resolve col notes

--- started_r_lang/project_data.r
+++ started_r_lang/project_data.r
@@ -10,22 +10,24 @@ length(mat_fac)
 col_notes = data.frame(df$Note)
 
 # Job on rows 4 of df
-row_four = col_notes[2,]
+row_four = col_notes[4,]
 
 
 #Define function to split all rows
 row_spliting <- function(row){
   row_list = strsplit(row_four, "#")
-  for (mat in row_list){
-    #print(mat)
+  print(row_list)
+  for (mat in row_list[[1]]){
+    
     label_matiere = strsplit(mat,"\\[")[[1]][1]
     notes_matiere = strsplit(strsplit(mat,"\\[")[[1]][2],"]")[[1]][1]
     devoirs = strsplit(strsplit(notes_matiere,":")[[1]][1],";")
     comp = strsplit(notes_matiere,":")[[1]][2]
-    print(label_matiere)
-    print(devoirs)
-    print(comp)
+    #print(label_matiere)
+    #print(devoirs)
+    #print(comp)
   }
+  return(c(label_matiere,devoirs,comp))
 }
 
 row_spliting(row_four)
--- started_r_lang/test.r
+++ started_r_lang/test.r
@@ -5,15 +5,15 @@ func_sep_notes <- function(devs,exam){
   dev2 = 0
   dev3 = 0
   
-  for(i in 1:length(devs)){
+  for(i in 1:length(devs[[1]])){
     if(i == 1){
-      dev1 = as.numeric(devs[i])
+      dev1 = as.numeric(devs[[1]][i])
     }
     else if(i == 2){
-      dev2 = as.numeric(devs[i])
+      dev2 = as.numeric(devs[[1]][i])
     }
     if(i == 3){
-      dev3 = as.numeric(devs[i])
+      dev3 = as.numeric(devs[[1]][i])
     }
     else{
       next()
@@ -23,4 +23,32 @@ func_sep_notes <- function(devs,exam){
   return(c(dev1,dev2,dev3,comp))
 }
 
-func_sep_notes(c("18"), "20")
+
+
+################################"
+row_four = "#Math[14;10:12] #PC[9:12]  #Francais[14;14:15]  #SVT[16;11:17] #HG[16:14]  #Anglais[13;10:11]"
+
+mondf_g = data.frame()
+
+row_spliting <- function(row){
+  row_list = strsplit(row_four, "#")
+  
+  for (mat in row_list[[1]]){
+    label_matiere = strsplit(mat,"\\[")[[1]][1]
+    notes_matiere = strsplit(strsplit(mat,"\\[")[[1]][2],"]")[[1]][1]
+    devoirs = strsplit(strsplit(notes_matiere,":")[[1]][1],";")
+    comp = strsplit(notes_matiere,":")[[1]][2]
+   
+    matrix_item = matrix(func_sep_notes(devoirs, comp),byrow=F, ncol=4)
+    #print(matrix_item)
+    
+    mondf = data.frame(notes=matrix_item,row.names=label_matiere)
+    
+    mondf_g = rbind(mondf_g,mondf)
+    
+  }
+  print(mondf_g)
+}
+
+row_spliting(row_four)
+ 
