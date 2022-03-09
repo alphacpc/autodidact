672b9deec7560cb3fbff3dc26d674f45bd122a06
alphacpc <alphacpc@gmail.com>
2022-03-09 10:11:32 +0000

    updated file genDatasRef

--- Mysql/genDatasRef.py
+++ Mysql/genDatasRef.py
@@ -7,10 +7,13 @@ prix = 1000000;
 
 #LOOP FOR INSERT DATAS TO REF TABLE
 while i < 10 :
+    
     key = round(random.random()* 10**4);
-    my_tuple = (key,prix * i+ 500000)
+    
+    my_tuple = (key,prix * i+ 500000);
+    
     cursor.execute("INSERT INTO reference(id_ref, prix) VALUES(%s, %s)", my_tuple);
-
+    
     i+=1;
-print("dialeu na")
+
 conn.commit();
