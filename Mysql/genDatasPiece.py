2f97f12ef965bc893939850c4e77a1a1cd5ec096
alphacpc <alphacpc@gmail.com>
2022-03-09 13:01:37 +0000

    finish to generate datas for pieces

--- Mysql/genDatasPiece.py
+++ Mysql/genDatasPiece.py
@@ -1,9 +1,12 @@
+
 from connecToDB import conn, cursor;
+import time
+import random
 
 #DEFINE VAR
+i = 0;
 tabs_refs = [];
 tabs_category = ["Carosserie", "Mecanique","Electrique"]
-
 
 
 #GET ALL 
@@ -15,5 +18,19 @@ for ref in result:
 
 print(tabs_refs)
 
-cursor.execute("INSERT INTO piece(categorie,date_recup,id_ref) VALUES(%s,%s,%s)", ("mecanique","12.11.2020",9684));
+while i < 20:
+
+    #Generer des dates du 1-jan-2018 à la date actuel 
+    #1515452400=1-jan-2018
+    piece_date_recup = time.strftime('%d.%m.%Y', time.gmtime(random.randint(1515452400, int(time.time()))));
+
+    piece_categ = random.choice(tabs_category);
+    
+    piece_ref = random.choice(tabs_refs)
+    
+    print(piece_categ, piece_date_recup, piece_ref)
+    cursor.execute("INSERT INTO piece(categorie,date_recup,id_ref) VALUES(%s,%s,%s)", (piece_categ, piece_date_recup, piece_ref));
+
+    i += 1;
+
 conn.commit();
--- Mysql/test.py
+++ Mysql/test.py
@@ -1,6 +1,8 @@
-import time
-import random
 
-date_rec = time.strftime('%d/%m/%Y', time.gmtime(random.randint(1515452400,int(time.time()))))
 
-print(date_rec)
+
+
+
+
+
+
