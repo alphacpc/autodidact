abc01a88108e325130d1049b10bcf6d0dc32a0d0
alphacpc <alphacpc@gmail.com>
2022-03-18 10:18:40 +0000

    Added requete and ignore __pycache__ for Lemgui

--- Lemgui/main.py
+++ Lemgui/main.py
@@ -1,3 +1,17 @@
 from connnectDB import conn, cursor
+import docx
+ 
+# open connection to Word Document
+doc = docx.Document("Requetes.docx")
+ 
+# read in each paragraph in file
+result = [p.text for p in doc.paragraphs]
+
+print(result)
+
+print("hello")
+
+
+
 
 
--- Lemgui/test.py
+++ Lemgui/test.py
@@ -1,32 +1,54 @@
 listMenuExec = []
 listMenu = [
-    {"req1": "Lister les tous les agences"},
-    {"req2": "Lister tous les caissiers par ordre croissant de leur nom"},
-    {"req3": "Lister tous chef d’agence ainsi que le nom de l’agence"},
-    {"req4": "Lister les comptes de transaction de l’agence Plateau par ordre croissant du solde"},
-    {"req5": "Lister la somme des montants déposés par un caissier dans un compte de transaction de l’agence dont le chef est moussa dop par ordre croissant du montant"},
-    {"req6": "Lister les utilisateurs de l’agence Plateau"},
-    {"req7": "Lister le nombre de compte par agence"},
-    {"req8": "Lister les comptes affectés à l’utilisateur moussa diop durant le mois de Mai 2021"},
-    {"req9": "Lister les utilisateurs à qui on a affecté le compte numéro 001 durant année 2021"},
-    {"req10": "Lister le montant des transactions effectué par utilisateur et par date dans l’agence dont le numéro est 001"},
-    {"req11": "Lister le nombre d’affectation par utilisateur et numéro de compte durant le premier trimestre de l’année 2021 par ordre croissant de ce nombre d’affectation dans l’agence dont le numéro est 001"},
-    {"req12": "Lister les dépôts effectués et les retraits par jour dans l’agence dont le chef est moussa diop par ordre croissant du montant"},
-    {"req13": "Lister les montants de transactions et les frais associés effectués par l’utilisateur Assane Faye dans l’agence dont le chef est moussa diop ."},
-    {"req14": "Lister la somme des parts de l’agence, de l'état et de l’état des transactions par date dans l’agence dont le numéro est 001."},
-    {"req15": "Lister la somme des parts de l’agence et de l'état par agence durant deuxième de l’année 2021"},
-    {"req16": "Lister l’agence qui a fait le plus de transfert durant le mois de Juin 2021"},
-    {"req17": "Lister l’agence qui a fait le moins de transfert de dépôt le 10-08-2021"},
-    {"req18": "Lister l’agence qui a fait le retrait le plus grand durant le mois de MAI 221"},
-    {"req19": "Lister les agences qui n’ont pas fait de dépôt le 10-08-2021"},
-    {"req20": "Lister les noms utilisés par l’agence numéro 001 durant le mois de MAI 221"},
-    {"req21": "Lister le ou les clients qui ont effectué le dépôt le plus petit durant le mois de MAI 221"},
-    {"req22": "Lister le ou les clients qui ont effectué le plus de dépôt durant le mois de MAI 221"},
-    {"req23": "Lister les 5 agences qui ont effectué le plus de transactions durant l’année 2021"},
-    {"req24": "Lister les 5 agences dont le montant gagné (somme des frais gagnés sur les transactions) sont les plus faibles en 2021."},
-    {"req25": "Lister l’utilisateur qui fait le plus de transfert dans l’agence dont le chef est moussa diop"},
+    "Lister les tous les agences",
+    "Lister tous les caissiers par ordre croissant de leur nom",
+    "Lister tous chef d’agence ainsi que le nom de l’agence",
+    "Lister les comptes de transaction de l’agence Plateau par ordre croissant du solde",
+    "Lister la somme des montants déposés par un caissier dans un compte de transaction de l’agence dont le chef est moussa dop par ordre croissant du montant",
+    "Lister les utilisateurs de l’agence Plateau",
+    "Lister le nombre de compte par agence",
+    "Lister les comptes affectés à l’utilisateur moussa diop durant le mois de Mai 2021",
+    "Lister les utilisateurs à qui on a affecté le compte numéro 001 durant année 2021",
+    "Lister le montant des transactions effectué par utilisateur et par date dans l’agence dont le numéro est 001",
+    "Lister le nombre d’affectation par utilisateur et numéro de compte durant le premier trimestre de l’année 2021 par ordre croissant de ce nombre d’affectation dans l’agence dont le numéro est 001",
+    "Lister les dépôts effectués et les retraits par jour dans l’agence dont le chef est moussa diop par ordre croissant du montant",
+    "Lister les montants de transactions et les frais associés effectués par l’utilisateur Assane Faye dans l’agence dont le chef est moussa diop .",
+    "Lister la somme des parts de l’agence, de l'état et de l’état des transactions par date dans l’agence dont le numéro est 001.",
+    "Lister la somme des parts de l’agence et de l'état par agence durant deuxième de l’année 2021",
+    "Lister l’agence qui a fait le plus de transfert durant le mois de Juin 2021",
+    "Lister l’agence qui a fait le moins de transfert de dépôt le 10-08-2021",
+    "Lister l’agence qui a fait le retrait le plus grand durant le mois de MAI 221",
+    "Lister les agences qui n’ont pas fait de dépôt le 10-08-2021",
+    "Lister les noms utilisés par l’agence numéro 001 durant le mois de MAI 221",
+    "Lister le ou les clients qui ont effectué le dépôt le plus petit durant le mois de MAI 221",
+    "Lister le ou les clients qui ont effectué le plus de dépôt durant le mois de MAI 221",
+    "Lister les 5 agences qui ont effectué le plus de transactions durant l’année 2021",
+    "Lister les 5 agences dont le montant gagné (somme des frais gagnés sur les transactions) sont les plus faibles en 2021.",
+    "Lister l’utilisateur qui fait le plus de transfert dans l’agence dont le chef est moussa diop",
 ]
 
 
-for item in listMenu:
-    print(item)
+
+def showMenuG():
+    ind = 1;
+
+    for item in listMenu:
+        
+        print(ind, ":" ,item)
+        
+        ind += 1;
+
+def checkChoise(enter):
+
+    print(listMenu[int(enter) - 1])
+
+    if listMenu[int(enter) - 1] not in listMenuExec:
+        
+        listMenuExec.append(listMenu[int(enter) - 1])
+
+
+showMenuG();
+
+choices = input("\nVeuillez choisir une option : ")
+
+checkChoise(choices)
